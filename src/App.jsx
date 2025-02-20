7d8930951a87d3383090f64e098eea8a5cf5e1eb
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 18:16:35 +0530

    change some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import PropTypes from "prop-types";
-
 const App = () => {
-  const onCopy = () => {
-    alert("Don't copy me");
-  };
-
   return (
-    <div className="text-3xl font-bold underline" onCopy={onCopy}>
-      <Password isValid={false} />
+    <div>
+      <h1 className="text-3xl underline font-bold">Hello</h1>
     </div>
   );
-};
-
-const Valid = () => {
-  return <h1>Valid Password</h1>;
-};
-
-const InValid = () => {
-  return <h1>Invalid Password</h1>;
-};
-
-const Password = ({ isValid }) => {
-  return (
-    <>
-      <div>{isValid ? <Valid /> : <InValid />}</div>
-    </>
-  );
-};
-
-Password.propTypes = {
-  isValid: PropTypes.bool,
 };
 
 export default App;
