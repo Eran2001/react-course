935fa8e0063b7666e4d452ea425839390535a5c9
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 14:47:49 +0530

    resets all

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import PropTypes from "prop-types";
-
 const App = () => {
   return (
     <div>
-      <User>
-        <h1 className="border-2 border-amber-900">Hello New User</h1>
-      </User>
-      <User>
-        <h1>Hello Old User</h1>
-        <p>Welcome</p>
-      </User>
+      <h1>Hello</h1>
     </div>
   );
-};
-
-const User = ({ children }) => {
-  return (
-    <>
-      <div className="bg-white m-4">{children}</div>
-    </>
-  );
-};
-
-User.propTypes = {
-  children: PropTypes.node,
 };
 
 export default App;
