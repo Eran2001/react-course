1e4408b4ecaea1ab1abb86ed53083fd198b0ab89
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 18:24:14 +0530

    test useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1 className="text-3xl underline font-bold">Hello</h1>
+      <h2>Number = {count}</h2>
+      <button
+        onClick={() => setCount((prev) => prev + 1)}
+        className="border-2 border-amber-900 bg-amber-400"
+      >
+        Change Count
+      </button>
     </div>
   );
 };
