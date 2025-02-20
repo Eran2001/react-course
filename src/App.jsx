b47e8eea5ad44004d14874bbcf30de3252c9a6bb
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 21:04:22 +0530

    test useRef for manipulate DOM directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
+import { useRef } from "react";
+
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl underline font-bold">Hello</h1>
-      <User username="Eran" password="1234" />
-      <User>
-        <h2>Hello User!</h2>
-      </User>
-      <User>
-        <h2 className="text-yellow-800">Hello World!</h2>
-      </User>
+      <InputFocus />
     </div>
   );
 };
 
-const User = ({ username, password, children }) => {
+const InputFocus = () => {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    inputRef.current.focus();
+  };
+
   return (
-    <>
-      <h2 className="text-2xl">{username}</h2>
-      <h3 className="text-2xl">{password}</h3>
-      {children}
-    </>
+    <div>
+      <input ref={inputRef} type="text" placeholder="Type here..." />
+      <button onClick={handleFocus}>Focus Input</button>
+    </div>
   );
 };
 
