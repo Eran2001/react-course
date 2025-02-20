bf9af2f3119d2a4c3558eac014a13cd8145c28a8
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 13:15:46 +0530

    add simple list and rendered it dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
+  const myList = [1, 2, 3, 4, 5];
+
   return (
     <div>
-      <h1 className="text-2xl">Hello</h1>
+      <h1 className="text-3xl">Hello</h1>
+      {myList.map((item, index) => (
+        <ul key={index}>
+          <li>{item}</li>
+        </ul>
+      ))}
     </div>
   );
 };
