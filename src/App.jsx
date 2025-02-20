50284ebceef37426a3cab198e9c4888ad383cd32
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 21:41:38 +0530

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  const [data, setData] = useState([]);
 
-  useEffect(() => console.log("Hello"), [value]);
-
-  const handleCount = () => {
-    setValue((prev) => prev + 1);
-  };
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/"
+      );
+      const data = await response.json();
+      setData([data]);
+    }
+    getData();
+  }, []);
 
   return (
     <div>
       <h1 className="text-3xl underline font-bold">Hello</h1>
-      <h2 className="indent-8">{value}</h2>
-      <button className="border-2 p-2" onClick={handleCount}>
-        Increment
-      </button>
+      {data.map((todo, index) => (
+        <li className="indent-20" key={index}>
+          {todo.title}
+        </li>
+      ))}
     </div>
   );
 };
