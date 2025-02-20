f368abb710ca363c3746d63abbdac4024b6b2915
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 14:24:11 +0530

    add another compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import PropTypes from "prop-types";
-
 const App = () => {
-  const myList = [1, 2, 3, 4, 5];
-
   return (
     <div>
-      <h1 className="text-3xl">Hello</h1>
-      {myList.map((item, index) => (
-        <ul key={index}>
-          <li>{item}</li>
-        </ul>
-      ))}
-      <User name="Eran" occupation="Engineer" />
-      <User name="Hasa" occupation="Designer" />
+      <h1>Hello</h1>
+      <User />
     </div>
   );
 };
 
-const User = ({ name, occupation }) => {
+const User = () => {
   return (
     <>
-      <h1 className="text-3xl">My name is s{name}</h1>
-      <p className="text-2xl">I&apos;m a {occupation}.</p>
+      <h1>Hello</h1>
     </>
   );
-};
-
-User.propTypes = {
-  name: PropTypes.string,
-  occupation: PropTypes.string,
 };
 
 export default App;
