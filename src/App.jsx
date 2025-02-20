73a1d99178de91a149b31ec9bafca4185e385536
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 14:43:16 +0530

    chnage something

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,26 @@ import PropTypes from "prop-types";
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <User username="eran2001" password="1234" />
-      <User username="hasa0416" password="1234" />
       <User>
         <h1>Hello New User</h1>
       </User>
       <User>
         <h1>Hello Old User</h1>
+        <p>Welcome</p>
       </User>
     </div>
   );
 };
 
-const User = ({ username, password, children }) => {
+const User = ({ children }) => {
   return (
     <>
-      <h1 className="text-2xl bg-amber-400 w-42">{username}</h1>
-      <h2 className="text-2xl bg-amber-300 w-42">{password}</h2>
-      {children && <div className="bg-gray-200 p-4 m-2">{children}</div>}
+      <div className="bg-white m-4">{children}</div>
     </>
   );
 };
 
 User.propTypes = {
-  username: PropTypes.string,
-  password: PropTypes.string,
   children: PropTypes.node,
 };
 
