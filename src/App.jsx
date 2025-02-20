2c682b027febf7d7f14d07706fc83370b0918387
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 13:37:06 +0530

    add another prop

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,24 @@ const App = () => {
           <li>{item}</li>
         </ul>
       ))}
-      <User name="Eran" />
+      <User name="Eran" occupation="Engineer" />
+      <User name="Hasa" occupation="Designer" />
     </div>
   );
 };
 
-const User = ({ name }) => {
+const User = ({ name, occupation }) => {
   return (
     <>
-      <h1>{name}</h1>
+      <h1 className="text-3xl">My name is s{name}</h1>
+      <p className="text-2xl">I&apos;m a {occupation}.</p>
     </>
   );
 };
 
 User.propTypes = {
   name: PropTypes.string,
+  occupation: PropTypes.string,
 };
 
 export default App;
