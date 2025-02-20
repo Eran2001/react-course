40f00502e83a3b50d87d2d9f4fb168869d423650
Eran <eran.hasarali.2001@gmail.com>
2025-02-20 14:26:08 +0530

    add prop validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import PropTypes from "prop-types";
+
 const App = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <User />
+      <User username="eran2001" password="1234" />
+      <User username="hasa0416" password="1234" />
     </div>
   );
 };
 
-const User = () => {
+const User = ({ username, password }) => {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{username}</h1>
+      <h2>{password}</h2>
     </>
   );
+};
+
+User.propTypes = {
+  username: PropTypes.string,
+  password: PropTypes.string,
 };
 
 export default App;
