5e0fbb65d7a6488a02178d6e760e350b46c2a056
Eran <eran.hasarali.2001@gmail.com>
2025-02-21 14:08:33 +0530

    test a complex state with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from "react";
-import { userContext } from "./userContext";
-import CompoA from "./CompoA";
-import CompoB from "./CompoB";
+import { useReducer } from "react";
 
 const App = () => {
-  const [user, setUser] = useState("Eran Hasareli");
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
+  function counterReducer(state, action) {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + 1 };
+      case "DECREMENT":
+        return { count: state.count - 1 };
+      case "RESET":
+        return { count: 0 };
+      default:
+        return state;
+    }
+  }
 
   return (
-    <userContext.Provider value={{ user, setUser }}>
-      <h1 className="text-3xl underline font-bold">Hello</h1>
-      <CompoA />
-      <CompoB />
-    </userContext.Provider>
+    <div>
+      <h1 className="text-2xl font-bold underline">{state.count}</h1>
+      <button
+        className="border-2 border-black p-4"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        +
+      </button>
+      <button
+        className="border-2 border-black p-4"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        -
+      </button>
+      <button
+        className="border-2 border-black p-4"
+        onClick={() => dispatch({ type: "RESET" })}
+      >
+        Reset
+      </button>
+    </div>
   );
 };
 
