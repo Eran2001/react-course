d524426aba7a6aba60e436a405e6e60a2f96cddc
Eran <eran.hasarali.2001@gmail.com>
2025-02-21 14:51:18 +0530

    delete some

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import useFetch from "./useFetch";
-
 const App = () => {
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/posts");
-
   return (
     <div>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      {data &&
-        data.map((item) => {
-          return <li key={item.id}>{item.title}</li>;
-        })}
     </div>
   );
 };
