be361c87a3747a57adbfcc8144cfdff4613bc334
Eran <eran.hasarali.2001@gmail.com>
2025-02-21 15:11:26 +0530

    use useId hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useId } from "react";
+
 const App = () => {
+  const id = useId();
+
   return (
     <div>
       <h1 className="text-2xl font-bold underline">Hello</h1>
+      <label htmlFor={`${id}-fname`}>First Name: </label>
+      <input className="border-2" type="text" name="" id={`${id}-fname`} />
+      <br />
+
+      <label htmlFor={`${id}-lname`}>Last Name: </label>
+      <input className="border-2" type="text" name="" id={`${id}-lname`} />
     </div>
   );
 };
