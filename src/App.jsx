f5bdea60adf0bee4a86d0ea6863d793fcb9ebc80
Eran <eran.hasarali.2001@gmail.com>
2025-02-21 14:09:09 +0530

    clear all

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,7 @@
-import { useReducer } from "react";
-
 const App = () => {
-  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
-
-  function counterReducer(state, action) {
-    switch (action.type) {
-      case "INCREMENT":
-        return { count: state.count + 1 };
-      case "DECREMENT":
-        return { count: state.count - 1 };
-      case "RESET":
-        return { count: 0 };
-      default:
-        return state;
-    }
-  }
-
   return (
     <div>
-      <h1 className="text-2xl font-bold underline">{state.count}</h1>
-      <button
-        className="border-2 border-black p-4"
-        onClick={() => dispatch({ type: "INCREMENT" })}
-      >
-        +
-      </button>
-      <button
-        className="border-2 border-black p-4"
-        onClick={() => dispatch({ type: "DECREMENT" })}
-      >
-        -
-      </button>
-      <button
-        className="border-2 border-black p-4"
-        onClick={() => dispatch({ type: "RESET" })}
-      >
-        Reset
-      </button>
+      <h1 className="text-2xl font-bold underline">Hello</h1>
     </div>
   );
 };
