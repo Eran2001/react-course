1d4fcd0c3103475bf305f889fff76c4c72f25ca2
Eran <eran.hasarali.2001@gmail.com>
2025-02-21 13:28:29 +0530

    pass data from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { createContext, useState } from "react";
+import CompoA from "./CompoA";
+import CompoB from "./CompoB";
+
 const App = () => {
+  const userContext = createContext();
+
+  const [user, setUser] = useState("Eran Hasareli");
+
   return (
-    <div>
+    <userContext.Provider value={{ user, setUser }}>
       <h1 className="text-3xl underline font-bold">Hello</h1>
-    </div>
+      <CompoA />
+      <CompoB />
+    </userContext.Provider>
   );
 };
 
