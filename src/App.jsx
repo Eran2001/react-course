6d54ea4a7e6157cf5674472046aecdd1c75bb393
Eran <eran.hasarali.2001@gmail.com>
2025-02-22 17:29:42 +0530

    remove one div

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ const App = () => {
   return (
     <div className="border-2 border-black m-4 bg-teal-400">
       <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
-      <div>
-        <canvas id="canvas" width="300" height="300"></canvas>
-      </div>
     </div>
   );
 };
