5a82c9dce0c2225a145a3e8e4c58f21e21cc9a4d
Eran <eran.hasarali.2001@gmail.com>
2025-02-22 17:28:58 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-
 const App = () => {
   return (
     <div className="border-2 border-black m-4 bg-teal-400">
       <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
-      <DotLottieReact src="path/to/animation.lottie" loop autoplay />
+      <div>
+        <canvas id="canvas" width="300" height="300"></canvas>
+      </div>
     </div>
   );
 };
