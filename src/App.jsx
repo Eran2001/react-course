5a13f73211276f8c255804726cb08ae80adbfdec
Eran <eran.hasarali.2001@gmail.com>
2025-02-25 09:55:46 +0530

    add styling for table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 const App = () => {
   return (
-    <div className="main-div border-2 border-black m-4 bg-teal-400">
-      <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
-      <div className="input">
-        <input type="text" name="" placeholder="filter fines" id="" />
+    <>
+      <div className="main-div border-2 border-black m-4 bg-teal-400">
+        <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
+        <div className="input">
+          <input type="text" name="" placeholder="filter fines" id="" />
+        </div>
       </div>
-    </div>
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Era</td>
+              <td>20000</td>
+            </tr>
+            <tr>
+              <td>Hasa</td>
+              <td>10000</td>
+            </tr>
+            <tr>
+              <td>Neo</td>
+              <td>30000</td>
+            </tr>
+            <tr>
+              <td>Tara</td>
+              <td>40000</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
