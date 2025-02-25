b22a72a69b987b268d4ff8c71d29792f41956cf2
Eran <eran.hasarali.2001@gmail.com>
2025-02-25 09:46:04 +0530

    add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
-    <div className="border-2 border-black m-4 bg-teal-400">
+    <div className="main-div border-2 border-black m-4 bg-teal-400">
       <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
+      <input type="text" name="" placeholder="filter fines" id="" />
     </div>
   );
 };
