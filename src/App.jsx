625c7bcc77451f123f078df48c1bac26441fdc2d
Eran <eran.hasarali.2001@gmail.com>
2025-02-25 09:49:15 +0530

    add styling for input

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ const App = () => {
   return (
     <div className="main-div border-2 border-black m-4 bg-teal-400">
       <h1 className="text-2xl font-bold underline m-2 p-2">Hello</h1>
-      <input type="text" name="" placeholder="filter fines" id="" />
+      <div className="input">
+        <input type="text" name="" placeholder="filter fines" id="" />
+      </div>
     </div>
   );
 };
