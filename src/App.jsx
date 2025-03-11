3f140fc002d2715acb2e768b9c523d5dde916e0f
Eran <eran.hasarali.2001@gmail.com>
2025-03-11 12:41:14 +0530

    test useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import User from "./User";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Hello</h1>
       <User isLoggedIn={false} />
+      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </div>
   );
 };
