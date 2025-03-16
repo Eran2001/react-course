96c1fe01703a673ccc46598edc39330ce9d82504
Eran <eran.hasarali.2001@gmail.com>
2025-03-16 19:52:41 +0530

    add a para

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,26 @@ const FocusInput = () => {
   return (
     <div>
       <h1>Hello</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad quasi
+        accusantium distinctio, et optio quo nulla, voluptates provident facilis
+        assumenda incidunt cupiditate libero commodi, consectetur delectus. Illo
+        voluptatem reiciendis ut? Vel maiores quia dolorum ipsam minus, aperiam
+        debitis aliquid optio architecto cupiditate, consequatur magni odit
+        libero reprehenderit non laboriosam iste distinctio ipsa, similique
+        possimus. Distinctio numquam officiis ea necessitatibus quas. Delectus
+        autem voluptate sit dignissimos non deleniti facilis quidem maiores
+        explicabo nihil dolorem ratione aspernatur voluptatum porro incidunt
+        beatae, ea, ab fuga doloribus excepturi sunt voluptas sed ullam nostrum.
+        Qui! Cum nisi veniam aperiam enim vitae illum perspiciatis repellat.
+        Illo dicta tempora sunt pariatur nemo. Voluptas laudantium suscipit,
+        quae dolores mollitia quaerat alias vel perspiciatis asperiores beatae,
+        cupiditate delectus? Dignissimos! Impedit saepe reprehenderit quod
+        nostrum, odit laborum distinctio. Molestias voluptatum fugit ratione
+        laboriosam numquam accusamus a deleniti quibusdam qui reprehenderit.
+        Fuga aut ipsa incidunt numquam voluptates unde quas. Assumenda,
+        officiis.
+      </p>
     </div>
   );
 };
