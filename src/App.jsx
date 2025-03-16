c8f0d481ce73cc7489fe64959b41a43a71bd7d85
Eran <eran.hasarali.2001@gmail.com>
2025-03-16 14:11:00 +0530

    remove some

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useRef } from "react";
+
 
 const FocusInput = () => {
-  const inputRef = useRef(null);
 
-  const handleClick = () => {
-    inputRef.current.focus(); // Focus the input
-  };
 
   return (
     <div>
-      <input ref={inputRef} type="text" />
-      <button onClick={handleClick}>Focus</button>
+      <h1>Hello</h1>
     </div>
   );
 };
