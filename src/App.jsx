abd65174928bc111cd51ab24a42e7c582e9d2326
Eran <eran.hasarali.2001@gmail.com>
2025-03-16 19:56:13 +0530

    remove para

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ const FocusInput = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <p className="p-2">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad quasi
-        accusantium distinctio, et optio quo nulla, voluptates provident facilis
-        assumenda incidunt cupiditate libero commodi, consectetur delectus.
-      </p>
     </div>
   );
 };
