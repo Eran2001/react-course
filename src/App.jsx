1e8917367370e8485a2c965e6f263f8f7f31ec7a
Eran <eran.hasarali.2001@gmail.com>
2025-03-17 15:11:19 +0530

    add some css

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,30 @@ const FocusInput = () => {
   const { count, increment, decrement, reset } = useCounterStore();
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center bg-gray-900 h-screen text-white">
       <h1 className="text-3xl font-bold underline center m-4">
         Counter: {count}
       </h1>
-      <button
-        onClick={increment}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        +
-      </button>
-      <button
-        onClick={decrement}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        -
-      </button>
-      <button
-        onClick={reset}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Reset
-      </button>
+      <div className="flex flex-row gap-3">
+        <button
+          onClick={increment}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          +
+        </button>
+        <button
+          onClick={decrement}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          -
+        </button>
+        <button
+          onClick={reset}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
