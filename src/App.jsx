5db98bc56036bd4fd6ddce67343e286a22fce99d
Eran <eran.hasarali.2001@gmail.com>
2025-03-17 15:07:28 +0530

    add zustand state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import useCounterStore from "./store";
+
 const FocusInput = () => {
+  const { count, increment, decrement, reset } = useCounterStore();
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
-      <h2 className="w-full">World</h2>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline center m-4">
+        Counter: {count}
+      </h1>
+      <button
+        onClick={increment}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        +
+      </button>
+      <button
+        onClick={decrement}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        -
+      </button>
+      <button
+        onClick={reset}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Reset
+      </button>
     </div>
   );
 };
