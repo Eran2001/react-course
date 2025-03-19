bb31ed383c50021d6564ea38ae5730651a06d6a5
Eran <eran.hasarali.2001@gmail.com>
2025-03-19 18:32:27 +0530

    use custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 const Example = () => {
-  const [count, setCount] = useState(0);
+  const [load, setLoading] = useState(true);
 
-  return <div className="h-screen bg-gray-900 text-white"></div>;
+  return (
+    <div className="h-screen bg-gray-900 text-white">
+      <h1>{load ? <p>loading...</p> : <p>Nothing...</p>}</h1>
+    </div>
+  );
 };
 
 export default Example;
