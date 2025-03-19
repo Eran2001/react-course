5c2bf93dc5d3dfaba7831889f249e6e278edc1f7
Eran <eran.hasarali.2001@gmail.com>
2025-03-19 22:23:03 +0530

    test useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
-import { useReducer } from "react";
+import { createContext, useState } from "react";
 
-const initialState = { count: 0 };
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    case "reset":
-      return initialState;
-  }
-}
+export const UserContext = createContext();
 
 const Example = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [user, setUser] = useState("John Doe");
+
   return (
     <div className="h-screen bg-gray-900 text-white p-4">
-      <h1>Count: {state.count}</h1>
-      <button
-        onClick={() => dispatch({ type: "increment" })}
-        className="border border-green-400 p-1 m-2 cursor-pointer"
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => dispatch({ type: "decrement" })}
-        className="border border-green-400 p-1 m-2 cursor-pointer"
-      >
-        Decrement
-      </button>
-      <button
-        onClick={() => dispatch({ type: "reset" })}
-        className="border border-green-400 p-1 m-2 cursor-pointer"
-      >
-        Reset
-      </button>
+      <UserContext.Provider value={user}>
+        <h1>{user}</h1>
+      </UserContext.Provider>
     </div>
   );
 };
--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "./App";
+
 const User = () => {
+  const name = useContext(UserContext);
+
   return (
     <div>
-      <h1>User</h1>
+      <h1>{name}</h1>
     </div>
   );
 };
