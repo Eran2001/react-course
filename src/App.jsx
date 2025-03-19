faf00d2b1d7b2dede83c17f8610c9f0f0e54234f
Eran <eran.hasarali.2001@gmail.com>
2025-03-19 18:30:02 +0530

    remove useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Example = () => {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.log("Component mounted or updated!");
-
-    return () => {
-      console.log("Component will unmount (Cleanup)");
-    };
-  }, [count]);
-
-  return (
-    <div className="h-screen bg-gray-900 text-white">
-      <h1>Count: {count}</h1>
-      <button
-        className="border border-green-500 p-2 mt-2"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment
-      </button>
-    </div>
-  );
+  return <div className="h-screen bg-gray-900 text-white"></div>;
 };
 
 export default Example;
