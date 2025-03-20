2ae64c4cd42c48db707ae1825bd43f41f2689c6d
Eran <eran.hasarali.2001@gmail.com>
2025-03-20 15:04:12 +0530

    test useEffect

--- src/CompoA.jsx
+++ src/CompoA.jsx
@@ -1,49 +1,21 @@
-import { useRef } from "react";
+import { useState, useEffect } from "react";
 
 const CompoA = () => {
-  const inputRef = useRef(null);
-  const divRef = useRef(null);
+  const [count, setCount] = useState(3);
 
-  const colorRef = () => {
-    inputRef.current.style.color = "yellow";
-  };
-
-  const handleClick = () => {
-    if (divRef.current) {
-      divRef.current.style.backgroundColor = "#ff0f4f";
-      divRef.current.style.transform = "scale(1.1)";
-      divRef.current.style.transition = "all 0.3s ease";
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((prev) => prev - 1);
+    }, 2000);
+    if (count === 0) {
+      alert("Game Over!");
+      window.location.reload();
     }
-  };
-
-  const removeClick = () => {
-    if (divRef.current) {
-      divRef.current.style.backgroundColor = "";
-      divRef.current.style.transform = "scale(1)";
-      divRef.current.style.transition = "all 0.7s ease";
-    }
-  };
+  });
 
   return (
-    <div className="box" ref={divRef}>
-      <h1 className="text-4xl" ref={inputRef}>
-        Hello
-      </h1>
-      <button
-        className="block border border-white mt-2 px-2"
-        onClick={colorRef}
-      >
-        Click
-      </button>
-      <button className=" border border-white mt-2 px-2" onClick={handleClick}>
-        Click for Styles
-      </button>
-      <button
-        className="ml-2 border border-white mt-2 px-2"
-        onClick={removeClick}
-      >
-        Click for Remove Styles
-      </button>
+    <div className="box">
+      <h1 className="text-4xl">{count}</h1>
     </div>
   );
 };
