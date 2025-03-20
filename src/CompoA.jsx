4407c2240d031c99f2d2a05e74f207592b22f29d
Eran <eran.hasarali.2001@gmail.com>
2025-03-20 14:54:17 +0530

    add some styles using yseRef

--- src/CompoA.jsx
+++ src/CompoA.jsx
@@ -1,21 +1,48 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 const CompoA = () => {
-  const [name] = useState("Era");
   const inputRef = useRef(null);
+  const divRef = useRef(null);
 
-  const focusInput = () => inputRef.current.focus();
+  const colorRef = () => {
+    inputRef.current.style.color = "yellow";
+  };
+
+  const handleClick = () => {
+    if (divRef.current) {
+      divRef.current.style.backgroundColor = "#ff0f4f";
+      divRef.current.style.transform = "scale(1.1)";
+      divRef.current.style.transition = "all 0.3s ease";
+    }
+  };
+
+  const removeClick = () => {
+    if (divRef.current) {
+      divRef.current.style.backgroundColor = "";
+      divRef.current.style.transform = "scale(1)";
+      divRef.current.style.transition = "all 0.7s ease";
+    }
+  };
 
   return (
-    <div className="box">
-      <h1>CompoA</h1>
-      <h2>{name}</h2>
-      <input className="border border-white mt-2" ref={inputRef} type="text" />
+    <div className="box" ref={divRef}>
+      <h1 className="text-4xl" ref={inputRef}>
+        Hello
+      </h1>
       <button
         className="block border border-white mt-2 px-2"
-        onClick={focusInput}
+        onClick={colorRef}
       >
-        Focus
+        Click
+      </button>
+      <button className=" border border-white mt-2 px-2" onClick={handleClick}>
+        Click for Styles
+      </button>
+      <button
+        className="ml-2 border border-white mt-2 px-2"
+        onClick={removeClick}
+      >
+        Click for Remove Styles
       </button>
     </div>
   );
