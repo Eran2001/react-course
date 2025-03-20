950ec6a0d42103f89ec86faa92de926ea956b27c
Eran <eran.hasarali.2001@gmail.com>
2025-03-20 10:34:55 +0530

    change function name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const Example = () => {
+const App = () => {
   return (
     <div className="h-screen bg-gray-900 text-white p-4">
       <h1>App</h1>
@@ -6,4 +6,4 @@ const Example = () => {
   );
 };
 
-export default Example;
+export default App;
