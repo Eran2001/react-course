62b6e9a6af53255137936294d2dd7aed8c7070b2
Eran <eran.hasarali.2001@gmail.com>
2025-03-20 13:57:48 +0530

    test useRef

--- src/CompoA.jsx
+++ src/CompoA.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const CompoA = () => {
   const [name] = useState("Era");
+  const inputRef = useRef(null);
+
+  const focusInput = () => inputRef.current.focus();
 
   return (
     <div className="box">
       <h1>CompoA</h1>
       <h2>{name}</h2>
+      <input className="border border-white mt-2" ref={inputRef} type="text" />
+      <button
+        className="block border border-white mt-2 px-2"
+        onClick={focusInput}
+      >
+        Focus
+      </button>
     </div>
   );
 };
