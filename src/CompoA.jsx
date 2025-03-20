40aa0ea2523dd2010de5bf6848b45143ec1e2bb7
Eran <eran.hasarali.2001@gmail.com>
2025-03-20 22:08:05 +0530

    delete some codes

--- src/CompoA.jsx
+++ src/CompoA.jsx
@@ -1,31 +1,7 @@
-import { useState, useRef } from "react";
-
 const CompoA = () => {
-  const divRef = useRef(null);
-  const [isClicked, setIsClicked] = useState(false);
-
-  if (divRef.current) divRef.current.style.transition = "all 0.5s";
-
-  const handleClick = () => {
-    if (divRef.current) {
-      divRef.current.style.backgroundColor = isClicked ? "#ff0f4f" : "#fdc500";
-      divRef.current.style.color = isClicked ? "white" : "black";
-      divRef.current.textContent = isClicked
-        ? "Click me to change!"
-        : "Changed!";
-      setIsClicked(!isClicked);
-    }
-  };
-
   return (
     <div className="flex flex-col items-center gap-4">
-      <div
-        ref={divRef}
-        className="p-4 bg-[#ff0f4f] text-#fff rounded-lg cursor-pointer"
-        onClick={handleClick}
-      >
-        Click me to change!
-      </div>
+      <h1 className="text-3xl">Hello</h1>
     </div>
   );
 };
