55881c01a5824099c9ecc5ad73fb91f31386a2a9
Eran <eran.hasarali.2001@gmail.com>
2025-03-21 09:06:58 +0530

    add some setters

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [name, setName] = useState("E");
+  const [name, setName] = useState("");
 
   return (
     <div className="flex flex-col items-center bg-gray-900 text-white h-screen text-3xl">
       <h1>Hello {name}</h1>
-      <input
-        type="text"
-        value={name}
-        required
-        className="border border-white mt-2"
-      />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="name">Name:</label>
+
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className="border border-white mt-2 ml-2 caret-[#000] bg-white text-[#000]"
+        />
+        <input
+          className="block border border-white mt-2 p-2"
+          type="submit"
+          value="Submit"
+        />
+      </form>
     </div>
   );
 };
