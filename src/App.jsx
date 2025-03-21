b0e0eb8525f5ed2429bd4ed45af5f9eb002def77
Eran <eran.hasarali.2001@gmail.com>
2025-03-21 09:32:28 +0530

    clear all

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
-import { useState } from "react";
-
 const App = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const [submittedData, setSubmittedData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmittedData(formData);
-  };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
   return (
     <div className="flex flex-col items-center bg-gray-900 text-white h-screen text-3xl">
-      <h1>Hello {submittedData.name}</h1>
-      <h2>Email: {submittedData.email}</h2>
-      <h3>Password: {submittedData.password}</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          value={formData.name}
-          name="name"
-          onChange={handleChange}
-          required
-          className="border border-white mt-2 ml-2 caret-[#000] bg-white text-[#000]"
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          value={formData.email}
-          name="email"
-          onChange={handleChange}
-          required
-          className="border border-white mt-2 ml-2 caret-[#000] bg-white text-[#000]"
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          value={formData.password}
-          name="password"
-          onChange={handleChange}
-          required
-          className="border border-white mt-2 ml-2 caret-[#000] bg-white text-[#000]"
-        />
-        <input
-          className="block border border-white mt-2 p-2"
-          type="submit"
-          value="Submit"
-        />
-      </form>
+      <h1>Hello</h1>
     </div>
   );
 };
