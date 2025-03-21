2ccc11093941884cc7e268579731843cec76b60f
Eran <eran.hasarali.2001@gmail.com>
2025-03-21 09:02:54 +0530

    add some state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("Mount");
-
-    return () => {
-      console.log("Unmount");
-    };
-  }, [count]);
+  const [name, setName] = useState("E");
 
   return (
     <div className="flex flex-col items-center bg-gray-900 text-white h-screen text-3xl">
-      <h1>Hello</h1>
-      <p>{count}</p>
-      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+      <h1>Hello {name}</h1>
+      <input
+        type="text"
+        value={name}
+        required
+        className="border border-white mt-2"
+      />
     </div>
   );
 };
