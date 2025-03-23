93c72a2439df1d84c1555bd5d6e79ea32e29b505
Eran <eran.hasarali.2001@gmail.com>
2025-03-23 17:43:21 +0530

    add a new paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl underline font-bold">Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+        accusantium quisquam mollitia, ex aspernatur consequatur facilis animi
+        nobis, dolorem beatae cum quia alias veritatis quos nisi quod nostrum
+        sit laborum! Culpa in iste mollitia provident. Alias quos tempora
+        nostrum vitae aspernatur est reiciendis dolore voluptatem temporibus.
+        Qui rerum eos alias accusantium fugit fugiat, ex dignissimos, impedit
+        veniam commodi nostrum sit.
+      </p>
     </div>
   );
 };
