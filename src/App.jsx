d16321ccaa81c686ebd06852cd324395d1b85c9e
Eran <eran.hasarali.2001@gmail.com>
2025-03-23 17:53:49 +0530

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
+
 const App = () => {
   const data = ["Apple", "Banana", "Orange", "Grapes", "Mango"]; // Array with 5 values
   const [search, setSearch] = useState("");
 
-  // Filter the array based on the search input
   const filteredData = data.filter((item) =>
     item.toLowerCase().includes(search.toLowerCase())
   );
