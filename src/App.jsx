eb706795f7ed12600d44a7a4cde882c78ce2dc0d
Eran <eran.hasarali.2001@gmail.com>
2025-03-23 22:10:37 +0530

    fetch some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [fetchData, setFetchData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+
+        if (!response.ok) throw new Error("Error");
+
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  });
+
   return (
     <div>
       <h1>Hello</h1>
