ca9c74db20461bc03ba86fee9640f9d9bf3d5ed4
Eran <eran.hasarali.2001@gmail.com>
2025-03-23 17:57:00 +0530

    add search func

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ const App = () => {
     item.toLowerCase().includes(search.toLowerCase())
   );
 
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="p-2 border border-gray-300 text-black rounded"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <ul className="mt-4">
+        {filteredData.length > 0 ? (
+          filteredData.map((item, index) => <li key={index}>{item}</li>)
+        ) : (
+          <li>No matching results</li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
