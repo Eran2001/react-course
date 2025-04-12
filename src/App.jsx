5e188ef30c657f845cc3ff1ec29c1562e6ab20d7
Eran <eran.hasarali.2001@gmail.com>
2025-04-12 21:27:37 +0530

    add model compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Example from "./Modal";
+
 const App = () => {
+  const [role] = useState("owner");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>
-      <h1 className="font-bold underline text-2xl">Hello</h1>
+    <div className="relative w-full">
+      <div
+        className={`flex justify-center items-center h-full transition-all duration-300 ${
+          isModalOpen ? "blur-xl" : ""
+        }`}
+      >
+        {role === "owner" && (
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            Add
+          </button>
+        )}
+      </div>
+
+      {isModalOpen && (
+        <div className="absolute inset-0 z-50">
+          <Example onClose={() => setIsModalOpen(false)} />
+        </div>
+      )}
     </div>
   );
 };
