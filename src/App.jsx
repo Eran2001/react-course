b210e7bbe7717fdb9f9b08291ae17047f33ce4b0
Eran <eran.hasarali.2001@gmail.com>
2025-04-13 09:24:35 +0530

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import { useState } from "react";
 import Example from "./Modal";
 
 const App = () => {
-  const [role] = useState("owner");
+  const [role, setRole] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="relative w-full">
+    <div
+      className={`relative w-full h-screen ${role ? "bg-grey" : "bg-black"}`}
+    >
       <div
         className={`flex justify-center items-center h-full transition-all duration-300 ${
           isModalOpen ? "blur-xl" : ""
         }`}
       >
-        {role === "owner" && (
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Add
-          </button>
+        {role && (
+          <div className="flex flex-col items-center justify-center h-full">
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+            >
+              Add
+            </button>
+          </div>
         )}
+        <button
+          onClick={() => setRole(!role)}
+          className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+        >
+          Role
+        </button>
       </div>
 
       {isModalOpen && (
