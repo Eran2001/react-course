3e1841b022b5efa513e6d9f0ebec0a894c6bd626
Eran <eran.hasarali.2001@gmail.com>
2025-04-15 20:26:45 +0530

    remove classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="flex justify-center items-center h-screen font-bold max-sm:text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+      <h1>
         Hello
       </h1>
     </div>
