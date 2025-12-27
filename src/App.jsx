dc21f3f5f88d0b15d15706f9980792af0359de24
M Eran Hasareli <eran.hasarali.2001@gmail.com>
2025-12-27 18:35:07 +0530

    update app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="bg-slate-800 min-h-screen">
+      <h1 className="text-white">App</h1>
     </div>
   );
 };
